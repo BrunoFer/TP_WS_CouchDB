7bba6af724b7b1d8f95ba0dad1903d23db6859ed
BrunoFer <bferreira129@gmail.com>
2013-01-22 20:24:42 -0200

    Deletando arquivos não necessários

--- AplicacaoWS/src/aplicacao/MongoDB.java
+++
@@ -1,9 +0,0 @@
-package aplicacao;
-
-public class MongoDB {
-	
-	public static final String acessoBanco = "http://localhost:28017/";
-	public static final String nomeBanco = "bancodobrasil";
-	public static final String nomeColecao = "alunos";
-
-}
