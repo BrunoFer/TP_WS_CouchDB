ea0e6f60ca6eb5421e560c6a55a5b6e2437bb3f0
Guilherme Anselmo <guiherme.joseanselmo@gmail.com>
2013-01-26 17:56:25 -0200

    Caixa de Aluno cadastrado e limpar campos.

--- AplicacaoWS/src/tratador_eventos/TratadorEventosCadastro.java
+++ AplicacaoWS/src/tratador_eventos/TratadorEventosCadastro.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.*;
 
 import aplicacao.AcessoBanco;
 
@@ -26,20 +26,19 @@ public class TratadorEventosCadastro implements ActionListener {
 			//System.out.println("Voce clicou no botao salvar");
 			
 			if (janela.getTextoNome().getText().equals(""))
-				JOptionPane.showMessageDialog(null,
-						"Você deve preencher o campo nome!",
-						"Cadastro de Aluno", JOptionPane.INFORMATION_MESSAGE);
+				showMessageDialog(null, "Você deve preencher o campo nome!",
+						"Cadastro de Aluno", INFORMATION_MESSAGE);
 			else {
 				if (janela.getTextoIdade().getText().equals(""))
-					JOptionPane.showMessageDialog(null,
-							"Você deve preencher o campo idade!",
-							"Cadastro de Aluno",
-							JOptionPane.INFORMATION_MESSAGE);
+					showMessageDialog(null, "Você deve preencher o campo idade!",
+							"Cadastro de Aluno", INFORMATION_MESSAGE);
 				else {
 					String json = montarJson();
 					try {
-						
 						acessoBanco.setRegistro(json);
+						showMessageDialog(null, "Aluno Cadastrado com sucesso!", 
+								"Cadastro de Aluno", INFORMATION_MESSAGE);
+						janela.limparDados();
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
@@ -48,9 +47,7 @@ public class TratadorEventosCadastro implements ActionListener {
 
 		} else if (e.getSource() == janela.getBotaoLimpar()) {
 			// System.out.println("Voce clicou no botao limpar");
-			janela.getTextoNome().setText("");
-			janela.getTextoIdade().setText("");
-			janela.getTextoTel().setText("");
+			janela.limparDados();
 		}
 	}
 	
