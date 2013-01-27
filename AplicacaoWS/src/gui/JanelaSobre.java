996945eddc4c1aa7c4253f6ddb1a6ef7ca4701f4
BrunoFer <bferreira129@gmail.com>
2013-01-27 21:55:13 -0200

    Informações do software e desenvolvimento adicionada na janela Sobre

--- AplicacaoWS/src/gui/JanelaSobre.java
+++ AplicacaoWS/src/gui/JanelaSobre.java
@@ -5,6 +5,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -17,18 +18,43 @@ public class JanelaSobre extends JDialog{
 	
 	public JanelaSobre(){
 		setTitle("Sobre Application Student");
-		setSize(400, 300);
+		setSize(500,400);
 	}
 	
 	public void montarJanelaSobre(){
 		JPanel panel1 = new JPanel();
-	    tabs.addTab("Tab 1", panel1);   
-	    tabs.setMnemonicAt(0, KeyEvent.VK_1);   
-	      
-	    JPanel panel2 = new JPanel();   
-	    tabs.addTab("Tab 2", panel2);
-	    tabs.setMnemonicAt(1, KeyEvent.VK_2);   
-	      
+		
+		JLabel projeto = new JLabel();
+		String texto1 = "<html><b>Aplicação desenvolvida para a aula de WebServices.</b> <br><br>" +
+				"O objetivo da aplicação era utilizar um banco de dados relacional,porém<br>" +
+				"a comunicação da aplicação com o banco deveria ser usando os métodos <br>" +
+				"do Protocolo HTTP (GET,POST,PUT e DELETE), através do REST.<br><br>" +
+				"<b>Ferramentas utilizadas:</b> <br>" +
+				" <b>-</b> CouchDB: Banco de dados não relacional, da empresa Apache;<br>" +
+				" <b>-</b> Biblioteca HTTPClient, da empresa Apache;<br>" +
+				" <b>-</b> Biblioteca JSON;<br>" +
+				" <b>-</b> IDE Eclipse Indigo.</html>";
+		projeto.setText(texto1);
+		panel1.add(projeto);
+	    tabs.addTab("Projeto", panel1);   
+	    tabs.setMnemonicAt(0, KeyEvent.VK_1);
+	    
+	    
+	    JLabel desenvolvimento = new JLabel();
+		String texto2 = "<html><b>Desenvolvido por:</b><br><br>" +
+				"<b>Alunos:</b>Bruno F. da Costa e Guilherme J. A. Moreira<br>" +
+				"<b>Professor:</b>Rafael Alencar<br>" +
+				"<b>Curso:</b>Superior em Sistemas para Internet<br>" +
+				"<b>Disciplina:</b>WebServices<br>" +
+				"<b>Intituto Federal de Educação, Ciência e Tecnologia do Sudeste<br>" +
+				"de Minas Gerais - Câmpus Barbacena</b><br>" +
+				"<b>Data de Entrega:</b>05/02/2013</html>";
+		desenvolvimento.setText(texto2);
+		JPanel panel2 = new JPanel(); 
+		panel2.add(desenvolvimento);
+	    tabs.addTab("Desenvolvimento", panel2);
+	    tabs.setMnemonicAt(1, KeyEvent.VK_2);
+	    
 	    //Add the tabbed pane to this panel.   
 	    add(tabs);
 	    setVisible(true);
