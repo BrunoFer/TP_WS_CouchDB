23d13cfa21aab611ae2bfc510130bcc2fe0a8d8c
BrunoFer <bferreira129@gmail.com>
2013-01-31 21:15:43 -0200

    melhorando a visualização do código

--- AplicacaoWS/src/aplicacao/Aplicacao.java
+++ AplicacaoWS/src/aplicacao/Aplicacao.java
@@ -1,6 +1,5 @@
 package aplicacao;
 
-
 import gui.JanelaPrincipal;
 
 import java.io.File;
@@ -9,12 +8,10 @@ import java.io.IOException;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-
 public class Aplicacao {
 	
 	public static void main(String[] args) throws IOException{
 		AcessoBanco acessoBanco = new AcessoBanco();
-			
 		JanelaPrincipal janela = new JanelaPrincipal(acessoBanco);
 		
 		try {
@@ -29,12 +26,21 @@ public class Aplicacao {
 		janela.montarJanela();
 	}
 	
-	public static void setandoCaminhosImagens(JanelaPrincipal janela) throws IOException{
-		janela.setCaminhoImgEditar(new File(".").getCanonicalPath()+"/img/editar.png");
-		janela.setCaminhoImgExcluir(new File(".").getCanonicalPath()+"/img/excluir.png");
-		janela.setCaminhoImgCadastrar(new File(".").getCanonicalPath()+"/img/cadastro.png");
-		janela.setImagemFundo(new File(".").getCanonicalPath()+"/img/images.jpg");
-		janela.setIconeTitulo(new File(".").getCanonicalPath()+"/img/couchdb.jpg");
+	/**
+	 * Função utilizada para setar os caminhos das imagens utilizadas durante a execução da
+	 * aplicação.
+	 * 
+	 * @param janela
+	 */
+	public static void setandoCaminhosImagens(JanelaPrincipal janela){
+		try {
+			janela.setCaminhoImgEditar(new File(".").getCanonicalPath()+"/img/editar.png");
+			janela.setCaminhoImgExcluir(new File(".").getCanonicalPath()+"/img/excluir.png");
+			janela.setCaminhoImgCadastrar(new File(".").getCanonicalPath()+"/img/cadastro.png");
+			janela.setImagemFundo(new File(".").getCanonicalPath()+"/img/images.jpg");
+			janela.setIconeTitulo(new File(".").getCanonicalPath()+"/img/couchdb.jpg");
+		} catch (IOException e) {
+		}
 	}
 
 }
