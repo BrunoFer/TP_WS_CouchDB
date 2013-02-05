c6aae7443017987aed4a162887b14691d8df275b
BrunoFer <bferreira129@gmail.com>
2013-02-05 00:49:43 -0200

    código melhorado

--- AplicacaoWS/src/validacao_campos/ValidaData.java
+++ AplicacaoWS/src/validacao_campos/ValidaData.java
@@ -5,71 +5,64 @@ public class ValidaData {
 	public String validaData(String data) {
 		try {
 
-			String dia, mes, ano;
-			int diaint, mesint, anoint;
-			int verificarano;// variável usada para ver se o ano termina em 00
+			int dia, mes, ano;
+			int verificaAno;// variável usada para ver se o ano termina em 00
 			String verano;// variável usada para capturar os dois dígitos finais
 							// do ano
 			int bissexto = 0; // igual à zero = bissexto, diferente disto ano
 								// normal
 
-			dia = data.substring(0, 2);// captura apenas o dia
-			mes = data.substring(3, 5);// captura apenas o mês
-			ano = data.substring(6, 10);// captura apenas o ano
-
-			diaint = Integer.parseInt(dia);// transforma o valor de String para
-											// int
-			mesint = Integer.parseInt(mes);
-			anoint = Integer.parseInt(ano);
+			// captura os valores da string data e passa para inteiro
+			dia = Integer.parseInt(data.substring(0, 2));
+			mes = Integer.parseInt(data.substring(3, 5));
+			ano = Integer.parseInt(data.substring(6, 10));
 
 			// Verificar se ano é bissexto
-			int valorano = anoint % 4;// captura o valor do resto
+			int valorano = ano % 4;// captura o valor do resto
 
 			if (valorano == 0) {// Anos em que o resto será zero
 				verano = data.substring(8, 10);// captura os dois dígitos finais
 												// do ano
-				verificarano = Integer.parseInt(verano);// transforma verano em
+				verificaAno = Integer.parseInt(verano);// transforma verano em
 														// int
 				bissexto = 0;// aciona ano bissexto, senão tiver esta linha os
 								// anos que são bissexto mas não termina em 00,
 								// serão anos normais
-				if (verificarano == 0) {
+				if (verificaAno == 0) {
 					// ***validação de datas para anos com fim 00
-					valorano = anoint % 400;
+					valorano = ano % 400;
 					if (valorano == 0) {
 						bissexto = 0;// ano bissexto
 					} else {
 						bissexto = 1;// ano normal
 					}
 					// ***fim da validação de datas para anos com fim 00
-				}// fim do verificarano==0
+				}// fim do verificaAno==0
 			}// fim do valorano==0
 			else {// Anos em que o resto não será zero
 				bissexto = 1;// ano normal
 			}
 
-			// verificar validade do mês
-			if (mesint > 0 && mesint <= 12) {
-				// verificar se o mês é fevereiro
-				if (mesint == 02) {
-					if (diaint >= 30 && bissexto == 0) {// ano bissexto
+			// verifica validade do mês
+			if (mes > 0 && mes <= 12) {
+				// verifica se o mês é fevereiro
+				if (mes == 02) {
+					if (dia >= 30 && bissexto == 0) {// ano bissexto
 						return "dia";
-					} else if (diaint >= 29 && bissexto == 1) {// ano normal
-						return "dia";
-					} else {
-						return null;
-					}
-				} else if (mesint == 1 || mesint == 3 || mesint == 5
-						|| mesint == 7 || mesint == 8 || mesint == 10
-						|| mesint == 12) {
-					if (diaint > 31) {
+					} else if (dia >= 29 && bissexto == 1) {// ano normal
 						return "dia";
 					} else {
 						return null;
 					}
-				} else if (mesint == 4 || mesint == 6 || mesint == 9
-						|| mesint == 11) {
-					if (diaint > 30) {
+				} else if (mes == 1 || mes == 3 || mes == 5 || mes == 7
+						|| mes == 8 || mes == 10 || mes == 12) {
+					if (dia > 31) {
+						return "dia";
+					} else {
+						return null;
+					}
+				} else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+					if (dia > 30) {
 						return "dia";
 					} else {
 						return null;
